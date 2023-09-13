b43861efa74f649d5128141307026a00dabf105c
Ilkka Kylmäniemi <ilkka.kylmaniemi@metropolia.fi>
2023-09-13 10:41:19 +0300

    eka resolver

--- src/api/resolvers/categoryResolver.ts
+++ src/api/resolvers/categoryResolver.ts
@@ -1,1 +1,11 @@
 // TODO: categoryResolver
+import {Category} from '../../interfaces/Category';
+import categoryModel from '../models/categoryModel';
+
+export default {
+  Query: {
+    categories: async () => {
+      return await categoryModel.find();
+    },
+  },
+};
