6fd4d44885b94c7344e0f54d1d5d2bbc0a47f0d0
Ilkka Kylmäniemi <ilkka.kylmaniemi@metropolia.fi>
2023-09-15 11:39:47 +0300

    upload

--- src/api/models/animalModel.ts
+++ src/api/models/animalModel.ts
@@ -24,6 +24,31 @@ const animalSchema = new Schema<Animal>({
     enum: ['Male', 'Female'],
     required: true,
   },
+  image: {
+    type: String,
+    required: true,
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: {
+      type: String,
+      enum: ['Point'],
+      required: true,
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+      validate: {
+        validator: (coordinates: number[]) => {
+          return coordinates.length === 2;
+        },
+        message: 'Coordinates must be an array of two numbers',
+      },
+    },
+  },
 });
 
 export default model<Animal>('Animal', animalSchema);
--- src/interfaces/Animal.ts
+++ src/interfaces/Animal.ts
@@ -1,11 +1,15 @@
 import {Document} from 'mongoose';
 import {Species} from './Species';
+import {Point} from 'geojson';
 
 interface Animal extends Document {
   animal_name: string;
   species: Species;
   birthdate: Date;
   gender: 'Female' | 'Male';
+  image: string;
+  thumbnail: string;
+  location: Point;
 }
 
 interface TestAnimal {
