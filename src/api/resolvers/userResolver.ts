033fede84fabc8a364c386c2d0600619629bed8b
Ilkka Kylmäniemi <ilkka.kylmaniemi@metropolia.fi>
2023-09-20 11:59:08 +0300

    autentikointi

--- src/api/resolvers/userResolver.ts
+++ src/api/resolvers/userResolver.ts
@@ -1,10 +1,10 @@
 export default {
   Query: {
     users: async () => {
-      console.log(`${process.env.AUTH_URL}/users`);
       const response = await fetch(`${process.env.AUTH_URL}/users`);
       const users = await response.json();
-      return users;
+      console.log(users);
+      return users.data;
     },
   },
 };
