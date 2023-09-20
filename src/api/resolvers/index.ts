e124deb8d81c9aba6a00c9bc3c70a55e851d4cd9
Ilkka Kylmäniemi <ilkka.kylmaniemi@metropolia.fi>
2023-09-20 11:57:30 +0300

    autentikointi

--- src/api/resolvers/index.ts
+++ src/api/resolvers/index.ts
@@ -1,5 +1,11 @@
 import animalResolver from './animalResolver';
 import speciesResolver from './speciesResolver';
 import categoryResolver from './categoryResolver';
+import userResolver from './userResolver';
 
-export default [categoryResolver, speciesResolver, animalResolver];
+export default [
+  categoryResolver,
+  speciesResolver,
+  animalResolver,
+  userResolver,
+];
--- src/api/resolvers/userResolver.ts
+++ src/api/resolvers/userResolver.ts
@@ -1,6 +1,7 @@
 export default {
   Query: {
     users: async () => {
+      console.log(`${process.env.AUTH_URL}/users`);
       const response = await fetch(`${process.env.AUTH_URL}/users`);
       const users = await response.json();
       return users;
