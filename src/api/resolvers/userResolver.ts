43951871b5a52eeeba0fc787b1bc55dbd9b678a8
Ilkka Kylmäniemi <ilkka.kylmaniemi@metropolia.fi>
2023-09-22 09:42:35 +0300

    auth jatkuu

--- src/api/resolvers/userResolver.ts
+++ src/api/resolvers/userResolver.ts
@@ -1,10 +1,22 @@
+import fetchData from '../../functions/fetchData';
+import AuthMessageResponse from '../../interfaces/AuthMessageResponse';
+
 export default {
   Query: {
     users: async () => {
-      const response = await fetch(`${process.env.AUTH_URL}/users`);
-      const users = await response.json();
+      const users = await fetchData<AuthMessageResponse>(
+        `${process.env.AUTH_URL}/users`
+      );
       console.log(users);
       return users.data;
     },
   },
+  Mutation: {
+    login: async (_parent: undefined, args: {email: string, password: string}) => {
+      const options: RequestInit = {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(args),
+      }
+
 };
